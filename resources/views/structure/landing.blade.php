77eab1243785acf1a4df0ebc373b1e411e1a2041
Baptiste Loubet <59566277+baptisteloubet@users.noreply.github.com>
2020-12-15 17:24:13 +0100

    Home/Présentation/Produit ok back !

--- resources/views/structure/landing.blade.php
+++ resources/views/structure/landing.blade.php
@@ -1,1 +1,5 @@
-<a href="{{route('structure.acceuil')}}"> Page Acceuil </a>
+<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+<div class="text-center">
+    <a href="{{route('structure.acceuil')}}" class="btn btn-success "> Page Acceuil</a>
+</div>
