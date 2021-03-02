5e7383695000b7ac909726596c4061c56c2c8463
delpivarleandre <delpivarleandre@gmail.com>
2021-03-02 18:27:44 +0100

    bug résolu

--- resources/views/presentation/index.blade.php
+++ resources/views/presentation/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+<div class="container">
     <section>
         <div class="row col-lg-12 pt-16 justify-content-center">
             <div class="col-lg-6 mb-4 font-weight-bold text-4xl">
@@ -18,17 +18,17 @@
                     Morbi fringilla, dolor eu tristique suscipit, risus libero hendrerit velit,
                     in pretium nibh mauris sit amet lacus. Morbi aliquet libero in diam mattis,
                     et dignissim nisl ullamcorper. Etiam at tellus a ipsum gravida pulvinar ut vel sem.
-                    Sed molestie aliquam nibh, a volutpat ante fermentum et. Duis aliquet ac sem id eleifend.<br/>
+                    Sed molestie aliquam nibh, a volutpat ante fermentum et. Duis aliquet ac sem id eleifend.<br />
                     Nullam tempor, nunc lacinia porttitor volutpat,
                     sapien lectus pellentesque purus, sit amet pellentesque tellus nisl quis nulla.
                     Morbi fringilla, dolor eu tristique suscipit, risus libero hendrerit velit,
                     in pretium nibh mauris sit amet lacus. Morbi aliquet libero in diam mattis,
                     et dignissim nisl ullamcorper. Etiam at tellus a ipsum gravida pulvinar ut vel sem.
-                    Sed molestie aliquam nibh, a volutpat ante fermentum et. Duis aliquet ac sem id eleifend.<br/>
+                    Sed molestie aliquam nibh, a volutpat ante fermentum et. Duis aliquet ac sem id eleifend.<br />
                 </p>
             </div>
             <div class="col-lg-4">
-                <img src="{{asset('/img/titrepres.png')}}"/>
+                <img src="{{asset('/img/titrepres.png')}}" />
             </div>
         </div>
     </section>
@@ -37,7 +37,7 @@
             <div class="col-lg-3">
                 <div class="pb16">
                     <a>
-                        <img src="{{asset('/img/presentation.png')}}" class="mx-auto p-2 roundedborder col-lg-8" alt="Logo" data-original-title="" title="" aria-describedby="tooltip591651"/>
+                        <img src="{{asset('/img/presentation.png')}}" class="mx-auto p-2 roundedborder col-lg-8" alt="Logo" data-original-title="" title="" aria-describedby="tooltip591651" />
                     </a>
                 </div>
                 <p class="text-center orange sanchez ml-4 mr-4 pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -47,7 +47,7 @@
             <div class="col-lg-3">
                 <div class="pb16">
                     <a>
-                        <img src="{{asset('/img/presentation.png')}}" class="mx-auto p-2 roundedborder col-lg-8" alt="Logo" data-original-title="" title="" aria-describedby="tooltip591651"/>
+                        <img src="{{asset('/img/presentation.png')}}" class="mx-auto p-2 roundedborder col-lg-8" alt="Logo" data-original-title="" title="" aria-describedby="tooltip591651" />
                     </a>
                 </div>
                 <p class="text-center orange sanchez ml-4 mr-4 pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -57,7 +57,7 @@
             <div class="col-lg-3">
                 <div class="pb16">
                     <a>
-                        <img src="{{asset('/img/presentation.png')}}" class="mx-auto p-2 roundedborder col-lg-8" alt="Logo" data-original-title="" title="" aria-describedby="tooltip591651"/>
+                        <img src="{{asset('/img/presentation.png')}}" class="mx-auto p-2 roundedborder col-lg-8" alt="Logo" data-original-title="" title="" aria-describedby="tooltip591651" />
                     </a>
                 </div>
                 <p class="text-center orange sanchez ml-4 mr-4 pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -68,8 +68,8 @@
     </section>
     <section class="row col-lg-12 justify-content-center">
         <div class="pt-16 mb-48">
-            <a href="/produits" class="buttonsbis pl-8 pr-8 nounderline">DÉCOUVREZ NOS SERVICES</a>
+            <a href={{route('products.index')}} class="buttonsbis pl-8 pr-8 nounderline">DÉCOUVREZ NOS PRODUITS</a>
         </div>
     </section>
-    </div>
+</div>
 @endsection
